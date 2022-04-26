7945e6a6798271429c23e147078697c9ffe9e42a
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-04-26 10:42:00 -0400

    first commit

---
+++ src/services/work-api.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const url = 'http://localhost:3001/'
+
+export function getJobs(){
+    const response = axios.get(url)
+    return response
+}
+
+export function getJob(id){
+    const response = axios.get(url+id)
+    return response
+}
+
+export function deleteJob(id){
+    const response = axios.delete(url+id)
+    return response
+}
+
+export function createJob(add){
+    const response = axios.post(url, add)
+    return response
+}
+
+export function editJob(id, edit){
+    const response = axios.put(url+id, edit)
+    return response
+}
